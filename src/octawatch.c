3c17a95965c64c5bfcde15801a768d2091a7271a
Willdorf <william.g.armstrong@gmail.com>
2015-10-20 21:49:16 -0400

    added watchface image

--- src/octawatch.c
+++ src/octawatch.c
@@ -1,35 +1,78 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static Layer *s_layer;
+
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static uint8_t s_hour;
+static uint8_t s_min;
+static uint8_t s_sec;
+
+static void update_time(struct tm *tick_time) {
+	s_hour = tick_time->tm_hour;
+	s_min = tick_time->tm_min;
+	s_sec = tick_time->tm_sec;
+	layer_mark_dirty(s_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	update_time(tick_time);
+}
+
+static void draw_watchface(Layer *layer, GContext *ctx) {
+}
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
+
+	s_layer = layer_create(layer_get_bounds(window_get_root_layer(window)));
+	layer_add_child(window_get_root_layer(window), s_layer);
+	layer_set_update_proc(s_layer, draw_watchface);
+
+	//setup the background image
+	s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_OCTAWATCH);
+	s_background_layer = bitmap_layer_create(GRect(0,0,144,168));
+	bitmap_layer_set_background_color(s_background_layer, GColorClear);
+	bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+	bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
+	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 }
 
 static void window_unload(Window *window) {
+	gbitmap_destroy(s_background_bitmap);
+	bitmap_layer_destroy(s_background_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+	window = window_create();
+	window_set_window_handlers(window, (WindowHandlers) {
+			.load = window_load,
+			.unload = window_unload,
+			});
+	const bool animated = true;
+	window_stack_push(window, animated);
+
+	//Register with TickTimerService
+	tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+
+	//display the time right away
+	time_t start_time = time(NULL);
+	update_time(localtime(&start_time));
 }
 
 static void deinit(void) {
-  window_destroy(window);
+	window_destroy(window);
+	tick_timer_service_unsubscribe();
 }
 
 int main(void) {
-  init();
+	init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
-  app_event_loop();
-  deinit();
+	app_event_loop();
+	deinit();
 }

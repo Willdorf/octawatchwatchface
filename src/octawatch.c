47e07bb81c339ebce5556faee17d44d618c0fae4
Willdorf <william.g.armstrong@gmail.com>
2015-10-20 21:31:00 -0400

    initial project setup

---
+++ src/octawatch.c
@@ -0,0 +1,35 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+}
+
+static void window_unload(Window *window) {
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
